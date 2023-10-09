f38a83483c9a168a89e2b66e1d128632b100f792
LuisJ <rabadanLjesus13@gmail.com>
2023-10-09 13:41:43 +0200

    Actualizacion

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,9 +3,17 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   {
+    path: '', redirectTo: 'inicio', pathMatch: 'full'
+  },
+  {
     path: '',
     loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule)
+  },
+  {
+    path: 'inicio',
+    loadChildren: () => import('./inicio/inicio.module').then( m => m.InicioPageModule)
   }
+
 ];
 @NgModule({
   imports: [
