26fcdbc2772ce907af0d9781f8ca06e1bdd40f05
Jojo <cristian19102004@gmail.com>
2023-10-11 11:12:35 +0200

    A

--- src/app/inicio/sobre-nosotros/sobre-nosotros.page.ts
+++ src/app/inicio/sobre-nosotros/sobre-nosotros.page.ts
@@ -7,6 +7,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SobreNosotrosPage implements OnInit {
 
+  nombreFundador ='Lola Mento';
+  nombreEmpresa = 'Troll Tech';
+  fechaFundacion = '03/10/2023';
+  sede = 'Mongolia';
+
   constructor() { }
 
   ngOnInit() {
