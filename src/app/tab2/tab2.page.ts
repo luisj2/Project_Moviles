428f9d7f8ddbfd00e0fffa720e8724a5037cd174
LuisJ <rabadanLjesus13@gmail.com>
2023-10-11 10:40:38 +0200

    PullLuisJesus

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -3,10 +3,16 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-tab2',
   templateUrl: 'tab2.page.html',
-  styleUrls: ['tab2.page.scss']
+  styleUrls: ['tab2.page.scss'],
+  template: `
+    <h2>Sitios donde nos ubicamos</h2>
+    <ul>
+      <li *ngFor="let nombre of nombres">{{ nombre }}</li>
+    </ul>
+  `,
 })
 export class Tab2Page {
+  nombres: string[] = [''];
 
   constructor() {}
-
 }
